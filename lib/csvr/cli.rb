cc94b3d5bc6241696ec25928bacd150081ccea3e
Andrew Smith <a.smith@live.ca>
2015-06-25 11:22:19 -0400

    Mistakes in api

--- lib/csvr/cli.rb
+++ lib/csvr/cli.rb
@@ -8,13 +8,15 @@ module CSVR
 		def api 
 
 			outputs = []
-			outputs << "\t require 'csvr' "
+			outputs << "\trequire 'csvr' "
  			outputs << "\tcsvr = CSVR.open('path/to/file')"
- 			outputs << "\t(optional) csvr.headers = ['array','of','custom', 'headers]"
+ 			outputs << "\t(optional) csvr.headers = ['array','of', 'headers]"
 			outputs << "\t(optional) csvr.filters = ['array', 'of' 'strings' 'to' 'parse']"
 			outputs << "\tcsvr.create('db_name', 'table_name')"
 
+			puts "\n"
 			outputs.each { |x| puts x }
+			puts "\n"
 
 		end
 	end
